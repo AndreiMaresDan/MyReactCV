c4a5e77ea1bc858f03ca75b7ce9d787c2c9a8a6a
AndreiMaresDan <andrei.mares911@gmail.com>
2020-10-17 21:21:47 +0300

    about page done

--- my-react-cv/src/components v2/About.js
+++ my-react-cv/src/components v2/About.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import api from '../components/icons/api.svg';
+import computer from '../components/icons/computer.svg';
+import Skillcard from '../components v2/Skillcard';
+import puzzle from '../components/icons/puzzle.svg';
+
+const skills = [
+    {
+        icon:computer,
+        title:"Front-End Developer",
+        about:"I can create a working website using HTML/CSS/JS/REACT"
+    },
+    {
+        icon:api,
+        title:"Graphic Designer",
+        about:"I can design posters and edit images for different purposes."
+    },
+    {
+        icon:api,
+        title:"Political Analyst",
+        about:"Wether is about domestic or foreign politics, no problem remains unsolved"
+    },
+    {
+        icon:api,
+        title:"PR",
+        about:"I can manage online or offline adds or various events targeting the right public"
+    },
+    {
+        icon:api,
+        title:"Language Enthusiast",
+        about:"Currently I am fluent in English, but I am learning Arabic and also, I am open to anything new"
+    },
+    {
+        icon:puzzle,
+        title:"Good working ethic",
+        about:"My entire working activity resides on voluntary work and one NGO leadership"
+    }
+
+]
 
 const About = () => {
     return (
-        <div>
-
+        <div className="about">
+        <h6 className="about__intro">
+        Friendly future front-end developer, 
+        interested in geopolitics, foreign languages and cooking, I like to think that life is full of opportunities 
+        and it's never too late to learn something new.
+        </h6>
+            <div className="container about__container">
+                <h6 className="about__heading"> What I offer</h6>
+                <div className="row">
+                    {
+                        skills.map(skill=>
+                           <Skillcard skill={skill}/>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 };
