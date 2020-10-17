3379701f820f0cb2984a4a486452d0578e168896
AndreiMaresDan <andrei.mares911@gmail.com>
2020-10-17 17:17:25 +0300

    background and bars added

--- my-react-cv/src/App.js
+++ my-react-cv/src/App.js
@@ -1,10 +1,21 @@
-  
 import React from 'react';
+import Sidebar from './components v2/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Uff! Finally</h1>
+    <div className="app">
+      <div className="container">
+       <div className="row">
+        <div className="col-lg-3">
+          <div className="app__sidebar">
+            <Sidebar />
+          </div>
+        </div>
+        <div className="col-lg-9 app__main-content">
+       main 
+     </div>
+       </div>
+      </div>
     </div>
   );
 }
