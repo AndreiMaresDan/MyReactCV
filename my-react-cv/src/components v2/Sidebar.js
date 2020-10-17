0834a9da06d608d524665383f414f12e093722d7
AndreiMaresDan <andrei.mares911@gmail.com>
2020-10-17 18:25:10 +0300

    email and css

--- my-react-cv/src/components v2/Sidebar.js
+++ my-react-cv/src/components v2/Sidebar.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import facebook from '../components/icons/facebook.svg';
+import instagram from '../components/icons/instagram.svg';
+import github from '../components/icons/github.svg';
+import pin from '../components/icons/pin.svg';
+import tie from '../components/icons/tie.svg';
+import mightycoder from '../components/mightycoder.svg';
+import resume from '../components/resume.pdf';
+
 
 const Sidebar = () => {
+    const handleEmailMe = () => {
+        window.open("mailto:andrei.mares911@gmail.com")
+    }
     return (
-        <h1>Sidebar</h1>
+        <div className="sidebar">
+            <img src={mightycoder} alt="avatar" className="sidebar__avatar"/>
+            <div className="sidebar__name">Dan-Andrei <span>Mares</span></div>
+            <div className="sidebar__item sidebar__title">Student</div>
+            <a href={resume} download="resume.pdf">
+                <div className="sidebar__item sidebar__resume">
+                    <img src={tie} alt="resume" className="sidebar__icon"/> Download Resume
+                </div>
+            </a>
+            <figure className="sidebar__social-icons my-2">
+                <a href=""><img src={facebook} alt="facebook" className="sidebar__icon mr-3"/> </a>
+                <a href=""><img src={instagram} alt="instagram" className="sidebar__icon"/> </a>
+            </figure>
+            <div className="sidebar__contact">
+                <div className="sidebar__item sidebar__github">
+                    <a href=""><img src={github} alt="github" className="sidebar__icon mr-3"/>github</a>
+                </div>
+                <div className="sidebar__location">
+                    <img src={pin} alt="location" className="sidebar__icon mr-3"/>
+                    Cluj-Napoca, Romania
+                </div>
+                <div className="sidebar__item">andrei.mares911@gmail.com</div>
+                <div className="sidebar__item">0755.347.130</div>
+            </div>
+            <div className="sidebar__item sidebar__email" onClick={handleEmailMe}>email me</div>
+        </div>
 
     );
 };
